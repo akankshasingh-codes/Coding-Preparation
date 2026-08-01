f1f2a14ef3a8e83db02b0cfd0c02202e59a1d088
Akanksha Singh <akankshasingh82735@gmail.com>
2026-08-01 22:02:22 +0530

    Time: 99 ms (14.85%), Space: 9.6 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
-//memoization
-    int t[23][23];
+
+// take direct difference of courses
     int solve(int i , int j , vector<int>&nums)
     {
         
         if(i > j) return 0;
 
         if(i == j) return nums[i];
-        if(t[i][j] != -1)
-            return t[i][j];
-        int take_i = nums[i] + min( solve(i+2,j,nums), solve(i+1,j-1,nums));
-        int take_j = nums[j] + min( solve(i+1,j-1,nums), solve(i,j-2,nums));
+       
+        int take_i = nums[i] - solve(i+1,j,nums);
+        int take_j = nums[j] - solve(i,j-1,nums);
 
-        return t[i][j] =  max(take_i, take_j);
+        return max(take_i, take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
 
-        //recursion method 2
-
-         memset(t, -1,sizeof(t));
-        //thinking only about player1
-        int total = accumulate(nums.begin(), nums.end(),0);
-        int p1_score = 0, p2_score = 0;
-
-        p1_score = solve(0, nums.size()-1,nums);
-
-        p2_score = total - p1_score;
-
-        if(p1_score >= p2_score)
-            return true;
-
-        return false;
+        //recursion method 3
+        
+        return solve(0, nums.size()-1,nums) >= 0;
         //t.c = 0(2^n ==> take or not take)
         
     }
