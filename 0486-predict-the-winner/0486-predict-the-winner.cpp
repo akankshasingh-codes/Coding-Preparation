76969c0af2ae358dcada4f1c44c87f0e6b99cc94
Akanksha Singh <akankshasingh82735@gmail.com>
2026-08-01 22:03:52 +0530

    Time: 0 ms (100%), Space: 9.6 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-
+    int t[23][23];
 // take direct difference of courses
     int solve(int i , int j , vector<int>&nums)
     {
@@ -8,16 +8,18 @@ public:
         if(i > j) return 0;
 
         if(i == j) return nums[i];
+        if(t[i][j] != -1)
+            return t[i][j];
        
         int take_i = nums[i] - solve(i+1,j,nums);
         int take_j = nums[j] - solve(i,j-1,nums);
 
-        return max(take_i, take_j);
+        return t[i][j] = max(take_i, take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
 
         //recursion method 3
-        
+        memset(t, -1, sizeof(t));
         return solve(0, nums.size()-1,nums) >= 0;
         //t.c = 0(2^n ==> take or not take)
         
