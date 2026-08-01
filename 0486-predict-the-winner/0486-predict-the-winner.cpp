d9358cce1f75003b5a3fffa8f8a71a8812f6e523
Akanksha Singh <akankshasingh82735@gmail.com>
2026-08-01 21:56:44 +0530

    Time: 0 ms (100%), Space: 9.7 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+//memoization
+    int t[23][23];
     int solve(int i , int j , vector<int>&nums)
     {
+        
         if(i > j) return 0;
+
         if(i == j) return nums[i];
+        if(t[i][j] != -1)
+            return t[i][j];
         int take_i = nums[i] + min( solve(i+2,j,nums), solve(i+1,j-1,nums));
         int take_j = nums[j] + min( solve(i+1,j-1,nums), solve(i,j-2,nums));
 
-        return max(take_i, take_j);
+        return t[i][j] =  max(take_i, take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
 
-        //recursion method 1
+        //recursion method 2
 
+         memset(t, -1,sizeof(t));
         //thinking only about player1
         int total = accumulate(nums.begin(), nums.end(),0);
         int p1_score = 0, p2_score = 0;
@@ -25,6 +32,7 @@ public:
             return true;
 
         return false;
+        //t.c = 0(2^n ==> take or not take)
         
     }
 };
