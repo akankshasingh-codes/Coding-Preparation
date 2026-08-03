53b70d898a4649f7cbbc8261416569a928e876b4
Akanksha Singh <akankshasingh82735@gmail.com>
2026-08-03 22:44:16 +0530

    Time: 19 ms (74.18%), Space: 136.2 MB (65.5%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
      
-     vector<int>dp;
-
-    int solve(vector<int>& stone, int i, int n) //using recursion + memozation
-    {
-        if( i >= n)
-            return 0; //now one can choose any stone
-        if(dp[i] != -1)
-            return dp[i];
-
-        
-        int res = INT_MIN;
-
-        // when player take only one element
-        res = stone[i] - solve(stone, i+1, n);
-
-        if(i+1 < n) // when player take only starting two elements
-            res = max(res, stone[i] + stone[i+1] - solve(stone, i+2, n) );
-        
-        if(i+2 < n) // when player take only starting three elements
-            res  = max(res,stone[i] + stone[i+1] + stone[i+2] - solve(stone, i+3, n));
-        
-        
-        return dp[i] = res ;
-        
-    }
-    string stoneGameIII(vector<int>& stone) {
+    string stoneGameIII(vector<int>& stone) { //bottom up approach
         
         int n = stone.size();
-        dp.resize(n+1, -1);
-        int diff = solve(stone, 0,n);
+        vector<int>dp(n+1);
+
+        for(int i = n-1; i >= 0 ;i--)
+        {
+            // when player take only one element
+            dp[i] = stone[i] - dp [i+1];
+
+            if(i+2 <= n) // when player take only starting two elements
+            dp[i] = max(dp[i], stone[i] + stone[i+1] - dp[i+2]);
+
+            if(i+3 <= n) // when player take only starting three elements
+            dp[i]  = max(dp[i],stone[i] + stone[i+1] + stone[i+2] - dp[i+3]);
+        }
+
+        int diff = dp[0];
 
         if(diff > 0)
             return "Alice";
@@ -38,6 +26,10 @@ public:
             return "Bob";
         else
             return "Tie";
+
+            //t.c = 0(n)
+            //s.c = 0(1) due to recursion
+
         
     }
 };
